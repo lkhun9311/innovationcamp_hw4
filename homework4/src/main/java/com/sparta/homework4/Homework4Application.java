2e9176a55df323177bae8b9d8090ab7b4953a81e
dae-woo <123kdw@naver.com>
2022-08-09 19:40:04 +0900

    이미지 api 코드 / aws s3 property 추가

--- homework4/src/main/java/com/sparta/homework4/Homework4Application.java
+++ homework4/src/main/java/com/sparta/homework4/Homework4Application.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -14,10 +15,13 @@ public class Homework4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Homework4Application.class, args);
+
     }
 
     @PostConstruct
     public void started() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
+
+
 }
