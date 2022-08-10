f0df1f3d9cbd505b9c5e9e3d23aa7cf676fd5d1f
kwanghunlee <lkhun9311@naver.com>
2022-08-10 09:40:27 +0900

    Fix Contents 객체에서 id의 GenerationType을 IDENTITY로 수정

--- homework4/src/main/java/com/sparta/homework4/model/Contents.java
+++ homework4/src/main/java/com/sparta/homework4/model/Contents.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Entity
 public class Contents extends Timestamped {
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
