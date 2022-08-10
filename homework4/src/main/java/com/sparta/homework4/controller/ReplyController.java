c8bd55fb553687805ceefafd539d07a338e11aee
kwanghunlee <lkhun9311@naver.com>
2022-08-10 09:32:15 +0900

    Add 요구사항에 맞게 서비스 결과가 모두 status 200이 되도록 @ResponseStatus(value = HttpStatus.OK) 추가

--- homework4/src/main/java/com/sparta/homework4/controller/ReplyController.java
+++ homework4/src/main/java/com/sparta/homework4/controller/ReplyController.java
@@ -5,6 +5,7 @@ import com.sparta.homework4.model.Reply;
 import com.sparta.homework4.repository.ReplyRepository;
 import com.sparta.homework4.security.UserDetailsImpl;
 import com.sparta.homework4.service.ReplyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +16,13 @@ public class ReplyController {
     private final ReplyRepository ReplyRepository;
     private final ReplyService ReplyService;
 
+    @ResponseStatus(value = HttpStatus.OK)
     @GetMapping({"/api/reply/{postId}"})
     public List<Reply> getReply(@PathVariable Long postId) {
         return this.ReplyService.getReply(postId);
     }
 
+    @ResponseStatus(value = HttpStatus.OK)
     @PostMapping({"/api/reply"})
     public String createReply(@RequestBody ReplyRequestDto requestDto,
                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -33,6 +36,7 @@ public class ReplyController {
         }
     }
 
+    @ResponseStatus(value = HttpStatus.OK)
     @PutMapping({"/api/reply/{id}"})
     public String updateReply(@PathVariable Long id,
                               @RequestBody ReplyRequestDto requestDto,
@@ -47,6 +51,7 @@ public class ReplyController {
         }
     }
 
+    @ResponseStatus(value = HttpStatus.OK)
     @DeleteMapping({"/api/reply/{replyId}"})
     public String deleteReply(@PathVariable Long replyId,
                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
