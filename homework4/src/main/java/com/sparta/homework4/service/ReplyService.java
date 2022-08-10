48032f26668e0bb7bbdb268611f04399d22f4fb2
kwanghunlee <lkhun9311@naver.com>
2022-08-10 17:05:21 +0900

    Fix ReplyService에서 댓글 생성 관련 코드 수정함.

--- homework4/src/main/java/com/sparta/homework4/service/ReplyService.java
+++ homework4/src/main/java/com/sparta/homework4/service/ReplyService.java
@@ -1,33 +1,49 @@
 package com.sparta.homework4.service;
 
 import com.sparta.homework4.dto.ReplyRequestDto;
+import com.sparta.homework4.model.Contents;
 import com.sparta.homework4.model.Reply;
+import com.sparta.homework4.model.User;
+import com.sparta.homework4.repository.ContentsRepository;
 import com.sparta.homework4.repository.ReplyRepository;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import javax.transaction.Transactional;
 
+import com.sparta.homework4.repository.UserRepository;
+import com.sparta.homework4.util.response.ContentsNotFound;
 import com.sparta.homework4.util.response.Response;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReplyService {
+    private final UserRepository userRepository;
+    private final ContentsRepository contentsRepository;
     private final ReplyRepository ReplyRepository;
 
-    public List<Reply> getReply(Long postId) {
-        return this.ReplyRepository.findAllByPostidOrderByCreatedAtDesc(postId);
+    public List<Reply> getReply(Long ContentsId) {
+        return this.ReplyRepository.findAllByContentsIdOrderByCreatedAtDesc(ContentsId);
     }
 
     @Transactional
-    public Response<String> createReply(ReplyRequestDto requestDto, String username, Long userId) {
+    public Response<String> createReply(Long contentsId, ReplyRequestDto requestDto, String username, Long userId) {
+        Optional<User> byUserId = userRepository.findById(userId);
+        User user = byUserId.orElseThrow(() -> new UsernameNotFoundException("로그인이 필요합니다."));
+
+        Optional<Contents> byContentsId = contentsRepository.findById(contentsId);
+        Contents contents = byContentsId.orElseThrow(() -> new ContentsNotFound("해당 게시글이 삭제되었거나 존재하지 않습니다."));
+
         String replyCheck = requestDto.getReply();
         Reply reply;
         if (!replyCheck.contains("script") && !replyCheck.contains("<") && !replyCheck.contains(">")) {
-            reply = new Reply(requestDto, username, userId);
+            reply = new Reply(requestDto, username);
+            reply.mapToContentsAndUser(contents, user);
             this.ReplyRepository.save(reply);
             return Response.<String>builder().status(200).data("댓글 작성을 완료했습니다.").build();
         } else {
-            reply = new Reply(requestDto, username, userId, "xss 안돼요,, 하지마세요ㅠㅠ");
+            reply = new Reply("xss 안돼요,, 하지마세요ㅠㅠ", username);
             this.ReplyRepository.save(reply);
             return Response.<String>builder().status(200).data("xss 안돼요,,하지마세요ㅠㅠ").build();
         }
@@ -59,7 +75,11 @@ public class ReplyService {
         }
     }
 
-    public ReplyService(ReplyRepository ReplyRepository) {
+    public ReplyService(UserRepository userRepository,
+                        ContentsRepository contentsRepository,
+                        ReplyRepository ReplyRepository) {
+        this.userRepository = userRepository;
+        this.contentsRepository = contentsRepository;
         this.ReplyRepository = ReplyRepository;
     }
 }
