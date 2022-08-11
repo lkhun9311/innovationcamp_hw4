ce2dade93a6e2d1ecd9fac0d97b4f0e20296ebdf
kwanghunlee <lkhun9311@naver.com>
2022-08-11 14:03:06 +0900

    Add 해당 게시글에 달린 댓글 갯수 불러오도록 코드 추가함.

--- homework4/src/main/java/com/sparta/homework4/repository/ReplyRepository.java
+++ homework4/src/main/java/com/sparta/homework4/repository/ReplyRepository.java
@@ -1,12 +1,19 @@
 package com.sparta.homework4.repository;
 
+import com.sparta.homework4.model.ContentLike;
+import com.sparta.homework4.model.Contents;
 import com.sparta.homework4.model.Reply;
 
 import java.util.List;
+import java.util.Optional;
+
+import com.sparta.homework4.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findAllByContentsIdOrderByCreatedAtDesc(Long contentsId);
 
-    Long countByContentsId(Long id);
+//    Optional<Reply> findByReplyAndUser(Reply reply, User user);
+
+    Long countByContentsId(Long contentsId);
 }
