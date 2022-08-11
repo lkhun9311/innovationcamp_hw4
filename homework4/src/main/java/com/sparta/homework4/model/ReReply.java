f42c3a12a84d4b12cc39913c25dfaa88ffdd123a
kwanghunlee <lkhun9311@naver.com>
2022-08-12 01:58:08 +0900

    Add 대댓글 작성시 관련 정보가 ReReply, Reply, Contents, User 객체에 전달되도록 구현.

--- homework4/src/main/java/com/sparta/homework4/model/ReReply.java
+++ homework4/src/main/java/com/sparta/homework4/model/ReReply.java
@@ -1,16 +1,9 @@
 package com.sparta.homework4.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sparta.homework4.dto.ReReplyRequestDto;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
-import static javax.persistence.FetchType.LAZY;
-
-@Getter
-@RequiredArgsConstructor
 @Entity
 public class ReReply extends Timestamped {
     @Id
@@ -35,6 +28,22 @@ public class ReReply extends Timestamped {
     @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_rereply_user"))
     private User user;
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public Long getUserId() { return user.getId(); }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getReReply() {
+        return this.reReply;
+    }
+
+    public ReReply() {}
+
     public ReReply(ReReplyRequestDto requestDto, String username) {
         this.reReply = requestDto.getReReply();
         this.username = username;
@@ -49,9 +58,33 @@ public class ReReply extends Timestamped {
         this.reReply = requestDto.getReReply();
     }
 
-    public void mapToReplyAndContentsAndUser(Reply reply, Contents contents, User user) {
-        this.reply = reply;
-        this.contents = contents;
+    public void mapToUser(User user) {
         this.user = user;
+        user.mapToReReply(this);
+    }
+
+    public void mapToContents(Contents contents) {
+        this.contents = contents;
+        contents.mapToReReply(this);
+    }
+
+    public void mapToReply(Reply reply) {
+        this.reply = reply;
+        reply.mapToReReply(this);
+    }
+
+    public void mapToUserRemove(User user) {
+        this.user = user;
+        user.mapToReReplyRemove(this);
+    }
+
+    public void mapToContentsRemove(Contents contents) {
+        this.contents = contents;
+        contents.mapToReReplyRemove(this);
+    }
+
+    public void mapToReplyRemove(Reply reply) {
+        this.reply = reply;
+        reply.mapToReReplyRemove(this);
     }
 }
