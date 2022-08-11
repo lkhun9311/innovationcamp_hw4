321640f2a47f138d886c088ce6de0af0f398ef07
kwanghunlee <lkhun9311@naver.com>
2022-08-12 02:04:53 +0900

    Fix ReReplyService 코드 전반적으로 수정, 대댓글의 변경된 정보가 ReReply, Reply, Contents, User 객체에 전달되도록 구현, 에러 관련 메세지 내용이 이해되도록 수정함.

--- homework4/src/main/java/com/sparta/homework4/service/ReReplyService.java
+++ homework4/src/main/java/com/sparta/homework4/service/ReReplyService.java
@@ -10,11 +10,13 @@ import com.sparta.homework4.repository.ReReplyRepository;
 import com.sparta.homework4.repository.ReplyRepository;
 import com.sparta.homework4.repository.UserRepository;
 import com.sparta.homework4.util.response.ContentsNotFound;
+import com.sparta.homework4.util.response.ReReplyNotFound;
+import com.sparta.homework4.util.response.ReplyNotFound;
 import com.sparta.homework4.util.response.Response;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -45,10 +47,14 @@ public class ReReplyService {
         ReReply reReply;
         if(!reReplyCheck.contains("script") && !reReplyCheck.contains("<") && !reReplyCheck.contains(">")){
             reReply = new ReReply(requestDto, username);
-            reReply.mapToReplyAndContentsAndUser(reply, contents, user);
+            reReply.mapToUser(user);
+            reReply.mapToContents(contents);
+            reReply.mapToReply(reply);
+            reply.updateReReplyCount();
+            contents.updateReReplyCount();
             this.reReplyRepository.save(reReply);
 
-            return Response.<String>builder().status(200).data("댓글 작성을 완료했습니다.").build();
+            return Response.<String>builder().status(200).data("대댓글을 작성했습니다.").build();
         } else{
             reReply = new ReReply("xss 안돼요,, 하지마세요ㅠㅠ", username);
             return Response.<String>builder().status(200).data("xss 안돼요,,하지마세요ㅠㅠ").build();
@@ -56,36 +62,61 @@ public class ReReplyService {
     }
 
     @Transactional
-    public String update(Long id, ReReplyRequestDto requestDto, String username, Long userId) {
-        ReReply reReply = (ReReply)this.reReplyRepository.findById(id).orElseThrow(() -> {
-            return new IllegalArgumentException("존재하지 않습니다.");
-        });
+    public String update(Long rereplyId, ReReplyRequestDto requestDto, Long userId, Long contentsId, Long replyId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 게시글이 삭제되었거나 존재하지 않습니다."));
 
-        Long writerId = reReply.getUser().getId();
+        Contents contents = contentsRepository.findById(contentsId)
+                .orElseThrow(() -> new ContentsNotFound("해당 게시글이 삭제되었거나 존재하지 않습니다."));
+
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new ReplyNotFound("해당 댓글이 삭제되었거나 존재하지 않습니다."));
+
+        ReReply reReply = reReplyRepository.findById(rereplyId)
+                .orElseThrow(() -> new ReReplyNotFound("해당 대댓글이 삭제되었거나 존재하지 않습니다."));
+
+        Long writerId = reReply.getUserId();
+
         if (Objects.equals(writerId, userId)) {
             reReply.update(requestDto);
-            return "댓글 수정 완료";
+            return "대댓글을 수정했습니다.";
         } else {
-            return "작성한 유저가 아닙니다.";
+            return "대댓글의 작성자가 아닙니다.";
         }
     }
 
-    public String deleteReply(Long id, Long replyId, Long userId) {
-        Long writerId = ((ReReply)this.reReplyRepository.findById(id).orElseThrow(() -> {
-            return new IllegalArgumentException("게시글이 존재하지 않습니다.");
-        })).getUser().getId();
+    @Transactional
+    public String deleteReply(Long rereplyId, Long replyId, Long contentsId, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 게시글이 삭제되었거나 존재하지 않습니다."));
+
+        Contents contents = contentsRepository.findById(contentsId)
+                .orElseThrow(() -> new ContentsNotFound("해당 게시글이 삭제되었거나 존재하지 않습니다."));
+
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new ReplyNotFound("해당 댓글이 삭제되었거나 존재하지 않습니다."));
+
+        ReReply reReply = reReplyRepository.findById(rereplyId)
+                .orElseThrow(() -> new ReReplyNotFound("해당 대댓글이 삭제되었거나 존재하지 않습니다."));
+
+        Long writerId = reReply.getUserId();
 
         if (Objects.equals(writerId, userId)) {
-            this.reReplyRepository.deleteById(id);
-            return "댓글 삭제 완료";
+            this.reReplyRepository.deleteById(rereplyId);
+            reReply.mapToUserRemove(user);
+            reReply.mapToContentsRemove(contents);
+            reReply.mapToReplyRemove(reply);
+            reply.updateReReplyCount();
+            contents.updateReReplyCount();
+            return "대댓글을 삭제했습니다.";
         } else {
-            return "작성한 유저가 아닙니다.";
+            return "대댓글의 작성자가 아닙니다.";
         }
     }
 
     public ReReplyService(UserRepository userRepository,
-                        ContentsRepository contentsRepository,
-                        ReplyRepository replyRepository,
+                          ContentsRepository contentsRepository,
+                          ReplyRepository replyRepository,
                           ReReplyRepository reReplyRepository) {
         this.userRepository = userRepository;
         this.contentsRepository = contentsRepository;
