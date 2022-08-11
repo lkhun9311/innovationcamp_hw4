e19ec20d0a115778e8f6bfba571b307a2445b9d4
kwanghunlee <lkhun9311@naver.com>
2022-08-11 20:40:15 +0900

    Add ReplyService에 댓글 좋아요 관련 메소드(replyLike) 추가함.

--- homework4/src/main/java/com/sparta/homework4/service/ReplyService.java
+++ homework4/src/main/java/com/sparta/homework4/service/ReplyService.java
@@ -1,16 +1,13 @@
 package com.sparta.homework4.service;
 
 import com.sparta.homework4.dto.ReplyRequestDto;
-import com.sparta.homework4.model.Contents;
-import com.sparta.homework4.model.Reply;
-import com.sparta.homework4.model.User;
-import com.sparta.homework4.repository.ContentsRepository;
-import com.sparta.homework4.repository.ReplyRepository;
+import com.sparta.homework4.model.*;
+import com.sparta.homework4.repository.*;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import com.sparta.homework4.repository.UserRepository;
 import com.sparta.homework4.util.response.ContentsNotFound;
 import com.sparta.homework4.util.response.ReplyNotFound;
 import com.sparta.homework4.util.response.Response;
@@ -23,6 +20,7 @@ public class ReplyService {
     private final UserRepository userRepository;
     private final ContentsRepository contentsRepository;
     private final ReplyRepository ReplyRepository;
+    private final ReplyLikeRepository replyLikeRepository;
 
     public List<Reply> getReply(Long ContentsId) {
         return this.ReplyRepository.findAllByContentsIdOrderByCreatedAtDesc(ContentsId);
@@ -32,7 +30,7 @@ public class ReplyService {
     public Response<String> createReply(Long contentsId, ReplyRequestDto requestDto, String username, Long userId) {
         Optional<User> byUserId = userRepository.findById(userId);
         User user = byUserId
-                .orElseThrow(() -> new UsernameNotFoundException("로그인이 필요합니다."));
+                .orElseThrow(() -> new UsernameNotFoundException("해당 게시글이 삭제되었거나 존재하지 않습니다."));
 
         Optional<Contents> byContentsId = contentsRepository.findById(contentsId);
         Contents contents = byContentsId
@@ -40,6 +38,7 @@ public class ReplyService {
 
         String replyCheck = requestDto.getReply();
         Reply reply;
+
         if (!replyCheck.contains("script") && !replyCheck.contains("<") && !replyCheck.contains(">")) {
             reply = new Reply(requestDto, username);
             reply.mapToUser(user);
@@ -58,9 +57,12 @@ public class ReplyService {
     public Response<String> update(Long contentsId, ReplyRequestDto requestDto, Long userId, Long replyId) {
         Contents contents = this.contentsRepository.findById(contentsId)
                 .orElseThrow(() -> new ContentsNotFound("해당 게시글이 삭제되었거나 존재하지 않습니다."));
+
         Reply reply = this.ReplyRepository.findById(replyId)
                 .orElseThrow(() -> new ReplyNotFound("해당 댓글이 삭제되었거나 존재하지 않습니다."));
+
         Long writerId = reply.getUserId();
+
         if (Objects.equals(writerId, userId)) {
             reply.update(requestDto);
             return Response.<String>builder().status(200).data("댓글을 수정했습니다.").build();
@@ -73,7 +75,7 @@ public class ReplyService {
     public Response<String> deleteReply(Long contentsId, Long replyId, Long userId) {
         Optional<User> byUserId = userRepository.findById(userId);
         User user = byUserId
-                .orElseThrow(() -> new UsernameNotFoundException("로그인이 필요합니다."));
+                .orElseThrow(() -> new UsernameNotFoundException("해당 게시글이 삭제되었거나 존재하지 않습니다."));
 
         Optional<Contents> byContentsId = contentsRepository.findById(contentsId);
         Contents contents = byContentsId
@@ -81,27 +83,62 @@ public class ReplyService {
 
         Optional<Reply> byReplyId = this.ReplyRepository.findById(replyId);
         Reply reply = byReplyId
-                .orElseThrow(() -> new ReplyNotFound("해당 댓글이 삭제되었거나 댓글의 작성자가 아닙니다."));
+                .orElseThrow(() -> new ReplyNotFound("해당 댓글이 삭제되었거나 존재하지 않습니다."));
         Long writerId = byReplyId.get().getUserId();
 
         if (Objects.equals(writerId, userId)) {
             ReplyRepository.deleteById(replyId);
-
             reply.mapToUserRemove(user);
             reply.mapToContentsRemove(contents);
             contents.updateReplyCount();
-
             return Response.<String>builder().status(200).data("댓글을 삭제했습니다.").build();
         } else {
             return Response.<String>builder().status(200).data("댓글의 작성자가 아닙니다.").build();
         }
     }
 
+    @Transactional
+    public void replyLike(Long contentId, Long userId, Long replyId) {
+        Optional<Contents> byContentsId = contentsRepository.findById(contentId);
+        Contents contents = byContentsId
+                .orElseThrow(() -> new ContentsNotFound("해당 게시글이 삭제되었거나 존재하지 않습니다."));
+
+        Optional<User> byUserId = userRepository.findById(userId);
+        User user = byUserId
+                .orElseThrow(() -> new UsernameNotFoundException("로그인이 필요합니다."));
+
+        Optional<Reply> byReplyId = this.ReplyRepository.findById(replyId);
+        Reply reply = byReplyId
+                .orElseThrow(() -> new ReplyNotFound("해당 댓글이 삭제되었거나 존재하지 않습니다."));
+
+        Optional<ReplyLike> byReplyAndContentsAndUser = replyLikeRepository.findByReplyAndContentsAndUser(reply, contents, user);
+
+        ReplyLike replyLike = ReplyLike.builder().build();
+
+        if(byReplyAndContentsAndUser.isPresent()){
+            reply.discountLike(byReplyAndContentsAndUser.get());
+            contents.discountReplyLike(byReplyAndContentsAndUser.get());
+            user.discountReplyLike(byReplyAndContentsAndUser.get());
+            reply.updateLikeCount();
+            contents.updateReplyLikeCount();
+            replyLikeRepository.delete(byReplyAndContentsAndUser.get());
+        } else {
+            replyLike.mapToReply(reply);
+            replyLike.mapToContent(contents);
+            replyLike.mapToUser(user);
+            reply.updateLikeCount();
+            contents.updateReplyLikeCount();
+            replyLikeRepository.save(replyLike);
+        }
+    }
+
     public ReplyService(UserRepository userRepository,
                         ContentsRepository contentsRepository,
-                        ReplyRepository ReplyRepository) {
+                        ReplyRepository ReplyRepository,
+                        ReplyLikeRepository replyLikeRepository) {
         this.userRepository = userRepository;
         this.contentsRepository = contentsRepository;
         this.ReplyRepository = ReplyRepository;
+        this.replyLikeRepository = replyLikeRepository;
     }
 }
