47794ec10093beba2e46f11073a7522c40dbdde5
kwanghunlee <lkhun9311@naver.com>
2022-08-12 01:56:14 +0900

    Add ReReplyService에서 대댓글 관련 메세지 담을 객체 생성

---
+++ homework4/src/main/java/com/sparta/homework4/util/response/ReReplyNotFound.java
@@ -0,0 +1,7 @@
+package com.sparta.homework4.util.response;
+
+public class ReReplyNotFound extends RuntimeException {
+    public ReReplyNotFound(String message) {
+        super(message);
+    }
+}
