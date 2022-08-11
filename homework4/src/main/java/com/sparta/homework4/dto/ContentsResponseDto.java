1ae1cf1563a09e45bf994b41c7b0da06db92225b
kwanghunlee <lkhun9311@naver.com>
2022-08-11 13:50:41 +0900

    Fix ContentsResponseDto에 에노테이션 Getter를 추가, 에노테이션 Builder를 추가해 코드 축소함.

--- homework4/src/main/java/com/sparta/homework4/dto/ContentsResponseDto.java
+++ homework4/src/main/java/com/sparta/homework4/dto/ContentsResponseDto.java
@@ -2,54 +2,33 @@ package com.sparta.homework4.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sparta.homework4.model.Contents;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class ContentsResponseDto {
     private Long id;
     private String title;
     private String name;
     private String contents;
+    private Long countReply;
+    private Long contentLikeCount;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime createdAt;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime modifiedAt;
-    private Long countReply;
 
+    @Builder
     public ContentsResponseDto(Contents content, Long countReply) {
         this.id = content.getId();
         this.title = content.getTitle();
         this.name = content.getName();
         this.contents = content.getContents();
-        this.modifiedAt = content.getModifiedAt();
+        this.contentLikeCount = content.getContentLikeCount();
         this.countReply = countReply;
-    }
-
-    public static ContentsResponseDtoBuilder builder() {
-        return new ContentsResponseDtoBuilder();
-    }
-
-    public static class ContentsResponseDtoBuilder {
-        private Contents content;
-        private Long countReply;
-
-        ContentsResponseDtoBuilder() {
-        }
-
-        public ContentsResponseDtoBuilder content(Contents content) {
-            this.content = content;
-            return this;
-        }
-
-        public ContentsResponseDtoBuilder countReply(Long countReply) {
-            this.countReply = countReply;
-            return this;
-        }
-
-        public ContentsResponseDto build() {
-            return new ContentsResponseDto(this.content, this.countReply);
-        }
-
-        public String toString() {
-            return "ContentsResponseDto.ContentsResponseDtoBuilder(content=" + this.content + ", countReply=" + this.countReply + ")";
-        }
+        this.createdAt = content.getCreatedAt();
+        this.modifiedAt = content.getModifiedAt();
     }
 }
