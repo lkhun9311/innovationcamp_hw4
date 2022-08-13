cfa01796bf2dd51d956a43978d3eefe56b5e4cb4
kwanghunlee <lkhun9311@naver.com>
2022-08-13 15:15:47 +0900

    Add 대댓글 좋아요 객체(ReReplyLike) 생성함.

---
+++ homework4/src/main/java/com/sparta/homework4/model/ReReplyLike.java
@@ -0,0 +1,52 @@
+package com.sparta.homework4.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@Entity
+public class ReReplyLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_ReReplyLike_User"))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "contents_id", foreignKey = @ForeignKey(name = "FK_ReReplyLike_Contents"))
+    private Contents contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id", foreignKey = @ForeignKey(name = "FK_ReReplyLike_Reply"))
+    private Reply reply;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reRely_id", foreignKey = @ForeignKey(name = "FK_ReReplyLike_ReReply"))
+    private ReReply reReply;
+
+    public void mapToUser(User user){
+        this.user = user;
+        user.mapToReReplyLike(this);
+    }
+
+    public void mapToContent(Contents contents){
+        this.contents = contents;
+        contents.mapToReReplyLike(this);
+    }
+
+    public void mapToReply(Reply reply){
+        this.reply = reply;
+        reply.mapToReReplyLike(this);
+    }
+
+    public void mapToReReply(ReReply reReply){
+        this.reReply = reReply;
+        reReply.mapToReReplyLike(this);
+    }
+}
