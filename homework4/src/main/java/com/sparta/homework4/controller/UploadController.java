8cad3161a41a7f8f85286a32c4c663b07cdb7c28
kwanghunlee <lkhun9311@naver.com>
2022-08-13 15:34:44 +0900

    Fix UploadController에서 JWT를 활용해 유효한 유저만 본인의 게시글에 이미지를 업로드할 수 있도록 코드를 변경함.

--- homework4/src/main/java/com/sparta/homework4/controller/UploadController.java
+++ homework4/src/main/java/com/sparta/homework4/controller/UploadController.java
@@ -1,7 +1,11 @@
 package com.sparta.homework4.controller;
 
+import com.sparta.homework4.security.UserDetailsImpl;
 import com.sparta.homework4.service.S3Uploader;
+import com.sparta.homework4.util.response.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,9 +17,16 @@ public class UploadController {
 
     private final S3Uploader s3Uploader;
 
-    @PostMapping("/images")
+    @ResponseStatus(value = HttpStatus.OK)
+    @PostMapping("/api/contents/{contentId}/images")
     @ResponseBody
-    public String upload(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-        return s3Uploader.upload(multipartFile, "static");
+    public Response<String> upload(@RequestParam("images") MultipartFile multipartFile,
+                         @PathVariable(name = "contentId") Long contentId,
+                         @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        if (userDetails == null) {
+            return Response.success("로그인이 필요합니다.");
+        } else {
+            return this.s3Uploader.upload(multipartFile, "static", contentId, userDetails.getUsername());
+        }
     }
 }
