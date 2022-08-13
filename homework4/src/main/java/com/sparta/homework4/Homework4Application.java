2faca1472fcdb52b3c81df18d9e9a24c7794414c
kwanghunlee <lkhun9311@naver.com>
2022-08-13 14:38:42 +0900

    Delete Homework4Application에서 필요없는 import 삭제함.

--- homework4/src/main/java/com/sparta/homework4/Homework4Application.java
+++ homework4/src/main/java/com/sparta/homework4/Homework4Application.java
@@ -2,16 +2,10 @@ package com.sparta.homework4;
 
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
-import javax.servlet.MultipartConfigElement;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.util.unit.DataSize;
-import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @EnableJpaAuditing
